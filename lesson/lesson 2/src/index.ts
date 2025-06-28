1eb55d4aa1bd09fe96611a184a2c620aa646fc64
alex blade <aalexblade@gmail.com>
2025-06-28 22:32:39 +0300

    index properties

--- lesson/lesson 2/src/index.ts
+++ lesson/lesson 2/src/index.ts
@@ -1,8 +1,8 @@
 // Index Properties
 
-// type Goods = {
-//   [key: string]: number;
-// };
+type Goods = {
+  [key: string]: number;
+};
 
 // type Fruits = {
 //   apples: number;
@@ -13,20 +13,20 @@
 // type FrozenGoods = {
 //   iceCream: number;
 //   fish: number;
-//   berry: number;
+//   berry: number; 
 // };
 
-// const fruits: Goods = {
-//   apples: 15,
-//   banana: 30,
-//   orange: 30,
-// };
+const fruits: Goods = {
+  apples: 15,
+  banana: 30,
+  orange: 30,
+};
 
-// const frozenGoods: Goods = {
-//   iceCream: 25,
-//   fish: 30,
-//   berry: 10,
-// };
+const frozenGoods: Goods = {
+  iceCream: 25,
+  fish: 30,
+  berry: 10,
+};
 
 // const myDict: StringDictionary = {
 //   prop1: "value1",
